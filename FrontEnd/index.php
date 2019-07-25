1e12156441038d7b2bbe0a0fbec6550608f04a2d
nassimamara94 <github@94>
2019-07-25 15:53:01 +0200

    portfolio 25/07/2019 php procedural

---
+++ FrontEnd/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>portFolio - Nassim Amara</title>
+    <!-- lien Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- lien perso css-->
+    <link rel="stylesheet" href="../FrontEnd/css/style.css">
+    <!-- lien de mon icone-->
+    <link rel="icon" href="https://img.icons8.com/ios/420/nfc-n-filled.png" type="image/png" />
+</head>
+<body>
+    <div class="container-fluid">
+        <!-- j'ai créer deux div principaux (row) -->
+        <div class="row">
+            <!-- à l'intérieur de ma row,on met un col-md-6 afin d'avoir la page d'accueil en 2 parties principaux-->
+            <div class="col-md-6" id="block1">
+                <img src="image/523-723x723.jpg" alt="volcan">
+            </div>
+            <div class="col-md-6" id="block2">
+                <div class="row">
+                    <div class="col-md-12" id="sBlocka"></div>
+                </div>
+                <!-- je sépare une div en 4 partie -->
+                <div class="row">
+                    <div class="col-md-6" id="sBlockb"></div>
+                    <div class="col-md-6" id="sBlockc"></div>
+                    <div class="col-md-6" id="sBlockd"></div>
+                    <div class="col-md-6" id="sBlocke"></div>
+                </div>
+            </div>
+        </div>
+   
+    </div>
+
+</body>
+
+</html>
+</body>
+</html>
