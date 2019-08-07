d62b973450907e28668063c48eb01ecabd33ebd2
nassimamara94 <github@94>
2019-08-07 14:14:12 +0200

    modif site

--- include/init.php
+++ include/init.php
@@ -2,13 +2,13 @@
  // fichier d'initialisation du site
 
 // connexion à la BDD
-        $bdd = new PDO('mysql:host=localhost;dbname=site_perso', 'root', '', array(PDO::ATTR_ERRMODE => PDO:: ERRMODE_WARNING, PDO:: MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8'));
+        $bdd = new PDO('mysql:host=localhost;dbname=siteperso', 'root', '', array(PDO::ATTR_ERRMODE => PDO:: ERRMODE_WARNING, PDO:: MYSQL_ATTR_INIT_COMMAND =>'SET NAMES utf8'));
 
 //----------------------------SESSION---------------------
 session_start();
 
 //---------------------------CHEMIN-----------------------
-define("URL","http://site_perso/");
+define("URL","http://siteperso/");
 
 //--------- VARIABLES
 $error =''; // message d'erreur
