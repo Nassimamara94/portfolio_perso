31a69549f10e3191214ca991c1acd894c32c0582
nassimamara94 <github@94>
2019-09-02 15:57:19 +0200

    rajout de texte sur site du 02/09/2019

--- about.php
+++ about.php
@@ -6,32 +6,10 @@ require_once("include/header.php");
 <!-- main -->
 <main class="main_about">
 <header>
-          <nav class="navbar navbar-expand-lg navbar-dark">
-    <div class="container">
-      <a class="navbar-brand" href="#"><strong>Navbar</strong></a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home</a>
-          </li> 
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Profile</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
 
 </header>
 <!--Fin Main-->
-
+<h1>A propos</h1>
 <p>
   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium vel sit minima fugiat reprehenderit facere quia vero doloribus, provident in laborum quaerat nihil iste, animi, quam temporibus? Aut, assumenda excepturi.
 </p>
