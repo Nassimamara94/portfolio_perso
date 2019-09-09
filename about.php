bb8bb286c8b380cf97215e4f504a5119c5677edd
nassimamara94 <github@94>
2019-09-09 15:20:14 +0200

    modif site perso

--- about.php
+++ about.php
@@ -7,6 +7,26 @@ require_once("include/header.php");
  <main class="main_about">
 
 <h1>A propos</h1>
+
+
+<div id="services" class="service-area">
+    <div class="container">
+        <h2 class="block_title">En quelques mots</h2>
+        <div class="row">
+            <div class="col-md-1 col-sm-6"></div>
+            <div class="col-md-7 col-sm-6">
+            
+
+            <p class="desc"><img src="https://img.icons8.com/office/50/000000/quote-left.png" alt="icone" style="padding:10px;"> Nassim
+            , 29 ans, Développeur intégrateur web Junior en formation, passionné par l'informatique et par le web en particulier. <img src="https://img.icons8.com/office/50/000000/quote-right.png" style="padding:10px;" alt="icone"></p>
+               <!-- telecharger mon cv -->
+       
+
+            </div> 
+            
+            
+
+        </div>
 <p>
   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium vel sit minima fugiat reprehenderit facere quia vero doloribus, provident in laborum quaerat nihil iste, animi, quam temporibus? Aut, assumenda excepturi.
 </p>
--- formation.php
+++ formation.php
@@ -9,44 +9,67 @@ require_once("include/header.php");
 
 <div class="container-fluid">
 
-<div class="lorem">
+<!-- <div class="lorem"> -->
+ <div class="jumbotron jumbotron-fluid header">
+            <h1 class="mb-4">Mes formations <i class="fas fa-graduation-cap"></i></h1>
+        </div>
 
+        <!-- div entourant le tableau -->
+        <div class="text-center table-responsive table-hover container">
+    
+            <table class="table table-bordered table-striped table-light">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>Titre Formations</th>
+                        <th>Sous-titre</th>
+                        <th>Dates</th>
+                        <th>Descriptions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                                        <tr>
+                        <td>Intégrateur Développeur Web Junior</td>
+                        <td>LepoleS, Vitry Sur Seine (94)</td>
+                        <td>19/12/2018 - En cours</td>
+                        .row
+                        <td class="text-left"><p>Technologies :</p>
 
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, vero hic? Tempore eos a voluptatibus porro illum inventore voluptas delectus soluta, quam at aperiam illo, unde ipsam sint, excepturi impedit.
-</p>
-</div>
+<ul >
+	<li>HTML</li>
+	<li>CSS</li>
+	<li>JavaScript</li>
+	<li>Jquery</li>
+	<li>Bootstrap</li>
+	<li>PHP Procédural</li>
+	<li>PHP Objet</li>
+	<li>MySQL</li>
+</ul>
+
+                                        <tr>
+                        <td>BTS Système Electronique</td>
+                        <td>Lycée Gustave Eiffel, Cachan (94)</td>
+                        <td>Septembre 2009 - juin 2011</td>
+                        <td class="text-left"><i class="fas fa-check"></i>
+</td>
+                    </tr>
+                                        <tr>
+                        <td>Baccalauréat STI </td>
+                        <td>Lycée Adolphe Cherioux, Vitry Sur Seine (94)</td>
+                        <td>Septembre 2005 - juin 2009</td>
+
+                    </tr>
+                                    </tbody>
+            </table>
+            
+        </div> 
 </div>
 
 
 </main>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, incidunt! Dolore dolores atque quia totam fugit nemo, cupiditate est expedita, pariatur in impedit voluptates saepe et temporibus explicabo numquam eveniet!</p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, incidunt! Dolore dolores atque quia totam fugit nemo, cupiditate est expedita, pariatur in impedit voluptates saepe et temporibus explicabo numquam eveniet!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, incidunt! Dolore dolores atque quia totam fugit nemo, cupiditate est expedita, pariatur in impedit voluptates saepe et temporibus explicabo numquam eveniet!</p>
 
 <!-- j'appelle la page footer -->
 <?php 
